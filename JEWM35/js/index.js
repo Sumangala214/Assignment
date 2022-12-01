0bc4fc807cae3dba51e9a60a2aeedb99f95cf7b6
Sumangala214 <91739107+Sumangala214@users.noreply.github.com>
2022-12-01 11:47:39 +0530

    Add files via upload

---
+++ JEWM35/js/index.js
@@ -0,0 +1,13 @@
+// let a=["jspiders","spiders",123,657,"python","red"];
+// console.log(a);
+// // let res1=a.push("Raja","Rani");
+// // console.log(a);
+// // console.log(res1);
+// let res1=a.unshift("Raju","Rani");
+// console.log(res1);
+
+function m1()
+{
+    console.log("hello");
+}
+m1();
